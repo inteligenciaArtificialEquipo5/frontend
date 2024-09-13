f52fbe0524afe534575a0224eb2ee672cc38efaf
A01754412 <88682649+A01754412@users.noreply.github.com>
2024-09-13 12:34:10 -0600

    grafico del modelo cargado en la pagina de modelo

--- src/pages/ModeloPage.jsx
+++ src/pages/ModeloPage.jsx
@@ -1,10 +1,25 @@
+import { motion } from 'framer-motion';
+import styles from '../styles/styles-modelpage.module.scss';
+
+import modeloImagen from '../assets/images/ModeloArbol.jpeg'
 
 const ModeloPage = () => {
     return (
-        <div>
-        
-        </div>
-    )
+        <motion.section 
+            className={`container mt-2 p-5 ${styles.contenedorPrincipalDashboard}`} 
+            initial={{ opacity: 0, scale: 0.8 }} 
+            animate={{ opacity: 1, scale: 1 }} 
+            transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20,
+                duration: 0.5
+            }}>
+            
+            
+            <img src={modeloImagen} alt="Modelo Sin Fondo" className={`img-fluid ${styles.modeloImagen}`} />
+        </motion.section>
+    );
 }
 
-export default ModeloPage
+export default ModeloPage;
